8372919317c6931551dfae49ece829026ecc1cd1
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-03 19:11:30 +0900

    App.go内でコンポーネント化

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,52 @@ import axios from 'axios';
 // ローカルで立ち上げているモックサーバのURL
 const todoDataUrl = "http://localhost:3100/todos";
 
+// TodoTitleコンポーネントを作成
+// 見出しタグがh1, h2の場合は条件分岐を作成しておく
+// 親コンポーネントからtitle, asをpropsとして受け取る
+const TodoTitle = ({ title, as }) => {
+  // asがh1ならtitleタグはh1タグ
+  if (as === "h1") return <h1>{title}</h1>;
+
+  // asがh2ならtitleタグはh2タグ
+  if (as === "h2") return <h2>{title}</h2>;
+
+  // どちらでもなければpタグ
+  return <p>{title}</p>;
+};
+
+// TodoItemコンポーネントを作成
+// 親コンポーネントからTodoをpropsとして受け取る
+const TodoItem = ({ todo }) => {
+  return (
+    <li>
+
+      {/** Todoの内容 */}
+      {todo.content}
+      {/* Todoが完了の場合は「未完了リストへ」、未完了の場合は「完了リストへ」と表示するボタンを設置する */}
+      {/* 現時点でトグルボタンは機能していない */}
+      <button>{todo.done ? "未完了リストへ" : "完了リストへ"}</button>
+
+      {/* TODOの削除ボタンを作成しておくが、現状ではまだ完成していない */}
+      <button>削除</button>
+
+    </li>
+  );
+};
+
+// TODOリストコンポーネントを作成
+// 親コンポーネントからtodolistをpropsとして受け取る
+const TodoList = ({ todoList }) => {
+  return (
+  <ul>
+    {/** mapを使ってTodoアイテムをひとつづつ取り出す */}
+    {todoList.map((todo) => (
+      <TodoItem todo={todo} key={todo.id}/>
+    ))}
+  </ul>
+  );
+};
+ 
 function App() {
   // todoListは現在のTodoの状態を保存する変数
   // setTodoListはTodoの状態を変更する関数
@@ -46,10 +92,16 @@ function App() {
 
   return (
     <>
-      <h1>TODO進捗管理</h1>
+
+    {/**TodoTitleコンポーネントを使用する */}
+      <TodoTitle title="Todo進捗管理" as="h1"/>
+
       <textarea />
+
       <button>+ TODOを追加</button>
-      <h2>未完了TODOリスト</h2>
+
+      <TodoTitle title="未完了TODOリスト" as="h2"/>
+
       <ul>
         {inCompletedList.map((todo) =>(
           <li key={todo.id}>
@@ -63,7 +115,7 @@ function App() {
         ))}
       </ul>
 
-      <h2>完了TODOリスト</h2>
+      <TodoTitle title="完了リスト" as="h2"/>
       <ul>
         {completedList.map((todo) => (
           <li key={todo.id}>
@@ -76,6 +128,7 @@ function App() {
           </li>
         ))}
       </ul>
+      {/** Todoリストもコンポーネント化 */}
       <h2>TODOリスト</h2>
       <ul>
         {todoList.map((todo) => (
@@ -84,6 +137,7 @@ function App() {
           </li>
         ))}
       </ul>
+      <TodoList todoList={completedList}/>
     </>
   );
 }
