c44dd229b2180bb7bb24ff89ff2f57d182254f10
django_freeman <o.tampodo.t.18@gmail.com>
2022-05-06 16:35:35 +0900

    TodoList用のコンポーネントを追加

---
+++ src/components/TodoList.js
@@ -0,0 +1,35 @@
+import { List } from "@chakra-ui/react";
+
+import { TodoTitle } from "./TodoTitle";
+import { TodoItem } from "./TodoItem";
+
+// TodoList用のコンポーネント
+// 親コンポーネントから色々とpropsを受け取る
+export const TodoList = ({
+    title,
+    as,
+    fontSize,
+    todoList,
+    toggleTodoListItemStatus,
+    deleteTodoListItem
+}) => {
+    return (
+        <>
+            {todoList.length !== 0 && (
+                <>
+                    <TodoTitle title={title} as={as} fontSize={fontSize} mt="12"/>
+                    <List w="full">
+                        {todoList.map((todo) => (
+                            <TodoItem
+                                todo={todo}
+                                key={todo.id}
+                                toggleTodoListItemStatus={toggleTodoListItemStatus}
+                                deleteTodoListItem={deleteTodoListItem}
+                            />
+                        ))}
+                    </List>
+                </>
+            )}
+        </>
+    );
+};
